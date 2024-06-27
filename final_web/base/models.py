738620886fcb32b7ed8e2c56231b4145e39f52d7
mariaminare <mariami.nareshelashvili.1@btu.edu.ge>
2024-06-27 21:14:13 +0400

    photos

--- final_web/base/models.py
+++ final_web/base/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Player(models.Model):
+        player_id = models.AutoField(primary_key=True)
+        first_name = models.CharField(max_length=100)
+        last_name = models.CharField(max_length=100)
+        date_of_birth = models.DateField()
+        nationality = models.CharField(max_length=100)
+        position = models.CharField(max_length=50)
+        current_club = models.CharField(max_length=100)
+        previous_clubs = models.TextField()
+        international_caps = models.IntegerField()  # IntegerField for whole numbers
+        goals_scored = models.IntegerField()  # IntegerField for whole numbers
+        height = models.FloatField()  # FloatField for floating-point numbers
+        weight = models.FloatField()  # FloatField for floating-point numbers
+        preferred_foot = models.CharField(max_length=10)
+        biography = models.TextField()
+        achievements = models.TextField()
+        profile_picture = models.URLField()
+
+
+        def __str__(self):
+                return f"{self.first_name} _ {self.last_name}"
--- final_web/base/views.py
+++ final_web/base/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Player
 
 # Create your views here.
 def home(request):
-    return render(request, 'base/home.html')
+    players=Player.objects.all()
+    context= {"players":players}
+    return render(request, 'base/home.html', context)
 
 
 def about(request):
     return render(request, 'base/about.html')
+def contact(request):
+    return render(request, 'base/contact.html')
