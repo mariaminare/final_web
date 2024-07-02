5dd785f16de76bcc809bf5f1d4568e92b861ecb0
mariaminare <mariami.nareshelashvili.1@btu.edu.ge>
2024-07-02 16:38:26 +0400

    search

--- final_web/base/models.py
+++ final_web/base/models.py
@@ -4,7 +4,7 @@ class Nation(models.Model):
         name=models.CharField(max_length=100)
         world_ranking=models.IntegerField()
         def __str__(self):
-                return f"{self.name},{self.world_ranking}"
+                return f"{self.name}"
 class Club(models.Model):
         name=models.CharField(max_length=100)
         foundation_date=models.DateField(null=True)
@@ -23,9 +23,9 @@ class Player(models.Model):
         first_name = models.CharField(max_length=100)
         last_name = models.CharField(max_length=100)
         date_of_birth = models.DateField()
-        nationality =models.ForeignKey(Nation, on_delete=models.SET("Unknown"))
+        nationality =models.ForeignKey(Nation, on_delete=models.SET("Unknown"), related_name='players')
         position = models.CharField(max_length=50)
-        current_club = models.ForeignKey(Club, on_delete=models.SET("Unknown"))
+        current_club = models.ForeignKey(Club, on_delete=models.SET("Unknown"), related_name='players')
         previous_clubs = models.TextField()
         international_caps = models.IntegerField()  # IntegerField for whole numbers
         goals_scored = models.IntegerField()  # IntegerField for whole numbers
@@ -39,4 +39,4 @@ class Player(models.Model):
         def __str__(self):
                 return f"{self.first_name} _ {self.last_name}"
 class User(AbstractUser):
-        players = models.ManyToManyField(Player, blank=True, related_name="players")
+        players = models.ManyToManyField(Player, blank=True, related_name="users")
--- final_web/base/views.py
+++ final_web/base/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Player,User
-
+from .models import Player,User,Club
+from django.db.models import Q
 # Create your views here.
 def home(request):
-    players=Player.objects.all()
-    context= {"players":players}
+    p=request.GET.get("p") if request.GET.get('p') != None else ""
+    #players=Player.objects.all()
+    players= Player.objects.filter(Q(first_name__icontains=p) | Q(last_name__icontains=p) |Q(nationality__name__icontains=p)| Q(current_club__name__icontains=p) )
+    #players=list(set(players)) ar mchirdeba, ertze met clubsa da erovnul gundshi ver iqnebian
+    clubs=Club.objects.all()
+    heading="Famous Players:"
+    context= {"players":players, "heading":heading, "clubs":clubs}
     return render(request, 'base/home.html', context)
 
 
@@ -17,7 +22,12 @@ def contact(request):
 
 def profile(request, pk):
     user= User.objects.get(id=int(pk))
-    players= user.players.all()
-    context={"players":players, "user":user}
+    p = request.GET.get("p") if request.GET.get('p') != None else ""
+    # players=user.objects.all()
+    players = user.players.filter(Q(first_name__icontains=p) | Q(last_name__icontains=p) | Q(nationality__name__icontains=p) | Q(current_club__name__icontains=p))
+    # players=list(set(players)) ar mchirdeba, ertze met clubsa da erovnul gundshi ver iqnebian
+    clubs = Club.objects.all()
+    heading="My Favourite Players:"
+    context={"players":players, "user":user, "heading":heading, "clubs":clubs}
     return render(request, 'base/profile.html',context)
 
